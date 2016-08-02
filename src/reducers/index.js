d9004abeba3bb650a23ff24b0b81291ff1939514
Alexandra Popescu <lixapopescu@gmail.com>
2016-08-02 17:47:08 +0200

    first commit

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,7 +1,11 @@
 import { combineReducers } from 'redux';
+import BooksReducers from './reducers_books';
+import ActiveBookReducer from './reducer_active_book';
 
 const rootReducer = combineReducers({
-  state: (state = {}) => state
+  // state: (state = {}) => state
+  books: BooksReducers,
+  activeBook: ActiveBookReducer
 });
 
 export default rootReducer;
---
+++ src/reducers/reducers_books.js
@@ -0,0 +1,8 @@
+export default function (){
+  return [
+    {title: 'Outlander', pages: 800},
+    {title: 'Me after you', pages: 300},
+    {title: 'John Grey', pages: 500},
+    {title: 'Last letter from your lover', pages: 320}
+  ];
+}
